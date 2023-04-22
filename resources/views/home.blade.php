6075de78daa8079c85b178dc800f01631bfe424a
Iuri Ribeiro <iuri.dias968@gmail.com>
2023-04-22 17:39:20 -0300

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.main')
+
+@section('title', 'Home - DimDim')
+
+@section('content')
+
+    <header>
+        <nav class="bg-green-700 h-20 p-5 flex items-center">
+            <a href="#" class="text-white text-3xl">DimDim</a>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+
+@endsection
